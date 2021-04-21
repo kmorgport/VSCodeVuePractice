9aac75ad4e9f4b9a36b6a48711d4ff1d7b77a8ee
Kole Morgan <kmorgan0057@gmail.com>
2021-04-20 22:43:49 -0500

    vue  conditional exercise practice

---
+++ condinAssess.js
@@ -0,0 +1,16 @@
+const app = Vue.createApp({
+    data(){
+        return{
+            enteredTask: '',
+            tasks: []
+        };
+    },
+    methods:{
+        addTask(){
+            this.tasks.push(this.enteredTask);
+        }
+    }
+
+})
+
+app.mount("#assignment")
